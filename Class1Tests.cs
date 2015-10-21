6dc45eca162d89b89aea011a34e13e7af1d1f011
ConyWu <abest805@gmail.com>
2015-10-21 15:48:54 +0800

    Update Class1Tests.cs

--- Class1Tests.cs
+++ Class1Tests.cs
@@ -1,5 +1,4 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ClassLibrary1;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +7,10 @@ using System.Threading.Tasks;
 using NSubstitute;
 
 
-namespace ClassLibrary1.Tests
+namespace CountProductsLibrary.Tests
 {
     [TestClass()]
-    public class Class1Tests
+    public class CountProductsTests
     {
         [TestMethod()]
         public void calculationOfCostTest()
@@ -19,16 +18,13 @@ namespace ClassLibrary1.Tests
             //Arrange
             var expected = new int[] { 6, 15, 24, 21 };
             int number = 3;
-            ICost stubCost = Substitute.For<ICost>();
+            IProperty stubCost = Substitute.For<IProperty>();
             stubCost.GetCost().Returns(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 });
 
-            IRevenue stubRevenue = Substitute.For<IRevenue>();
-            stubRevenue.GetRevenue().Returns(new int[] { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 });
-
-            Class1 target = new Class1(stubCost, stubRevenue);
+            CountProducts target = new CountProducts();
 
             //Act
-            var actual = target.calculationOfCost(number);
+            var actual = target.calculation(number, stubCost);
 
             //Assert
             CollectionAssert.AreEqual(expected, actual);
@@ -40,16 +36,14 @@ namespace ClassLibrary1.Tests
             //Arrange
             var expected = new int[] { 50, 66, 60 };
             int number = 4;
-            ICost stubCost = Substitute.For<ICost>();
-            stubCost.GetCost().Returns(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 });
 
-            IRevenue stubRevenue = Substitute.For<IRevenue>();
+            IProperty stubRevenue = Substitute.For<IProperty>();
             stubRevenue.GetRevenue().Returns(new int[] { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 });
 
-            Class1 target = new Class1(stubCost, stubRevenue);
+            CountProducts target = new CountProducts();
 
             //Act
-            var actual = target.calculationOfCost(number);
+            var actual = target.calculation(number, stubRevenue);
 
             //Assert
             CollectionAssert.AreEqual(expected, actual);
